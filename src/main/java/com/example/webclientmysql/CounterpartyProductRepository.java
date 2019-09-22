ae6455e0282ed54c976ff8c70e1d422ab6cebca8
siporqueno <strike1000001@gmail.com>
2019-09-22 15:36:00 +0300

    First working draft. Search criteria and front-end remains much to be desired.

--- src/main/java/com/example/webclientmysql/CounterpartyProductRepository.java
+++ src/main/java/com/example/webclientmysql/CounterpartyProductRepository.java
@@ -2,9 +2,14 @@ package com.example.webclientmysql;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface CounterpartyProductRepository extends CrudRepository<CounterpartyProduct, CounterpartyProductId> {
-
+List<CounterpartyProduct> findCounterpartyProductsByProductShortName(String productShortName);
+List<CounterpartyProduct> findCounterpartyProductsByCounterpartyShortName(String counterpartyShortName);
+List<CounterpartyProduct> findCounterpartyProductsByCounterpartyShortNameOrProductShortName(String counterpartyShortName, String productShortName);
+List<CounterpartyProduct> findCounterpartyProductsByCounterpartyShortNameAndProductShortName(String counterpartyShortName, String productShortName);
 }
