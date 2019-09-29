6e6bd4c0933457dda609d93802073960a88fc55a
siporqueno <strike1000001@gmail.com>
2019-09-29 11:35:02 +0300

    Changed in comment of CounterpartyProductRepository.java
    the word "userRepository" for the word "counterpartyProductRepository"

--- src/main/java/com/example/webclientmysql/CounterpartyProductRepository.java
+++ src/main/java/com/example/webclientmysql/CounterpartyProductRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// This will be AUTO IMPLEMENTED by Spring into a Bean called counterpartyProductRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface CounterpartyProductRepository extends CrudRepository<CounterpartyProduct, CounterpartyProductId> {
